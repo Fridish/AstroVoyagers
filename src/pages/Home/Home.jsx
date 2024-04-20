573bcee831d577f881b1ae1dcf920688bd4b22a6
William Davidson <wj.davidson@outlook.com>
2024-04-20 12:14:59 +0200

    added hero text with custom font and added icons component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,23 @@
 import Timeline from "@components/Timeline/Timeline";
 import styles from "./Home.module.css";
-import Button from "@components/Button/Button";
+import Typography from "@components/Typography/Typography";
+import Icons from "@components/Icons";
 
 const Home = () => {
   return (
     <main className={styles.container}>
       <section className={styles.hero}>
-        <div className={styles.canvas}>Astro Vayagers</div>
+        <Typography variant="h1" style={{ fontFamily: "porter-sans" }}>
+          Astro Voyagers
+        </Typography>
+        <Typography variant="h5">
+          SpaceX History, Rockets and Roadster
+        </Typography>
+        <Icons
+          variant={"planetStripesDots"}
+          color="black"
+          className={styles.planetStripesDots}
+        />
       </section>
       <Timeline />
     </main>
