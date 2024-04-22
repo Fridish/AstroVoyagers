4e80ea73927fa7a8a4cd2badc2a43b0fcb6965cd
William Davidson <wj.davidson@outlook.com>
2024-04-22 19:42:48 +0200

    fixed nav animation styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,47 +1,100 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Nav.module.css";
 import useWindowSize from "@hooks/useWindowSize";
 import Hamburger from "@components/Hamburger/Hamburger";
+import Logo from "../../../public/logo.svg?react";
+
+const List = ({ onClick }) => {
+  return (
+    <>
+      <Link onClick={onClick} to={"/rockets/falcon-1"}>
+        Falcon 1
+      </Link>
+      <Link onClick={onClick} to={"/rockets/falcon-9"}>
+        Falcon 9
+      </Link>
+      <Link onClick={onClick} to={"/rockets/falcon-heavy"}>
+        Falcon Heavy
+      </Link>
+      <Link onClick={onClick} to={"/rockets/starship"}>
+        Starship
+      </Link>
+    </>
+  );
+};
 
 const Nav = () => {
   const [showNav, setShowNav] = useState(false);
+  const [showClose, setShowClose] = useState(false);
+  const close = useRef(null);
   const size = useWindowSize();
 
   useEffect(() => {
     if (size.width >= 760 && showNav) {
       setShowNav(false);
+      setShowClose(false);
     }
   }, [size.width, showNav]);
 
-  const toggleNav = () => setShowNav((prev) => !prev);
+  const toggleNav = (show) => {
+    setShowNav(show);
+
+    if (!close.current) return;
+
+    if (show) {
+      setShowClose(show);
+      setTimeout(() => {
+        close.current.style.opacity = 1;
+      }, 10);
+    } else {
+      close.current.style.opacity = 0;
+      setTimeout(() => {
+        setShowClose(show);
+      }, 400);
+    }
+  };
+
+  const getNavWidth = () => {
+    return size.width < 400 ? `${size.width}px` : "400px";
+  };
 
   return (
     <nav role="navigation" className={styles.nav}>
       <div className={styles.header}>
-        <Link to={"/"}>Astro Voyagers</Link>
-        {size.width < 760 && (
-          <Hamburger onClick={toggleNav} aria-expanded={showNav} />
+        <Link onClick={() => toggleNav(false)} to={"/"}>
+          <Logo className={styles.logo} />
+        </Link>
+        {size.width < 760 ? (
+          <Hamburger
+            onClick={() => toggleNav(!showNav)}
+            aria-expanded={showNav}
+          />
+        ) : (
+          <div className={styles.horizontal}>
+            <List />
+          </div>
         )}
       </div>
-      <div
-        className={`${styles.list} ${
-          showNav && size.width < 760 ? styles.show : ""
-        }`}
-      >
-        <Link onClick={toggleNav} to={"/rockets/falcon-1"}>
-          Falcon 1
-        </Link>
-        <Link onClick={toggleNav} to={"/rockets/falcon-9"}>
-          Falcon 9
-        </Link>
-        <Link onClick={toggleNav} to={"/rockets/falcon-heavy"}>
-          Falcon Heavy
-        </Link>
-        <Link onClick={toggleNav} to={"/rockets/starship"}>
-          Starship
-        </Link>
-      </div>
+      {size.width < 760 && (
+        <div
+          className={styles.side}
+          style={{ width: showNav ? getNavWidth() : 0 }}
+        >
+          <div
+            ref={close}
+            onClick={() => toggleNav(false)}
+            style={{ display: showClose ? "block" : "none" }}
+            className={styles.closeSide}
+          />
+          <div
+            className={`${styles.background} ${showNav ? styles.skew : ""}`}
+          />
+          <div className={`${styles.verticle} ${showNav ? styles.show : ""}`}>
+            <List onClick={() => toggleNav(false)} />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
