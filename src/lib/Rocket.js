098cbe3e0c5204d1458bf99dc6abcfa2da499f63
Frida Fernhede <fernhedefrida@gmail.com>
2024-04-22 12:06:43 +0200

    Display all data

--- src/lib/Rocket.js
+++ src/lib/Rocket.js
@@ -3,17 +3,21 @@
     const RocketIds = {
     'falcon-1': {
         id: '5e9d0d95eda69955f709d1eb',
+        hero: 'https://www.nasaspaceflight.com/wp-content/uploads/2017/12/BCK_4062-1-1170x780.jpg',
     },
     'falcon-9':{
-        id:'5e9d0d95eda69973a809d1ec'
+        id:'5e9d0d95eda69973a809d1ec', 
+        hero: 'https://www.nasaspaceflight.com/wp-content/uploads/2017/12/BCK_4062-1-1170x780.jpg',
     },
-    starship: {id: '5e9d0d96eda699382d09d1ee'
+    'starship': {
+        id: '5e9d0d96eda699382d09d1ee', 
+        hero: 'https://www.nasaspaceflight.com/wp-content/uploads/2017/12/BCK_4062-1-1170x780.jpg',
     },
     'falcon-heavy': {
         id:'5e9d0d95eda69974db09d1ed',
         hero:'https://www.spacex.com/media/fh_performance.mp4',
     },
-    roadster: { 
+    'roadster': { 
         id:'5eb75f0842fea42237d7f3f4',
     },
 }
--- src/pages/Rocket/Rocket.jsx
+++ src/pages/Rocket/Rocket.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import styles from "./Rocket.module.css";
 import RocketIds from "../../lib/Rocket.js";
 import useFetch from "@hooks/useFetch";
+import Typography from "@components/Typography/Typography";
 
 const Rocket = () => {
   const { rocket } = useParams();
@@ -25,23 +26,85 @@ return <main className={styles.container}>
   <div>
    {/*  hero section */}
     <section className= {styles.hero}> 
-    <h1 className={styles.header1}> {rocketData.name} </h1>
+    <Typography variant="h1" style={{ fontFamily: 'porter-sans'}}> {rocketData.name} </Typography>
     <video className={styles.heroBackground} autoPlay loop muted src={ RocketIds[rocket].hero || ""} alt={rocketData.name} />
+    <img className={styles.heroBackground}  src={ RocketIds[rocket].hero } alt={"Hero img of " [rocketData.name]} />
     </section>
 
  {/*  weight, height, diameter */}
+ <div className={styles.details}>
     <section className={styles.RocketDetails}>
-      <h2> {rocketData.height.meters}meters </h2>
-      <h2> {rocketData.diameter.meters}meters</h2>
-      <h2> {rocketData.mass.kg}kg</h2>
+      <Typography variant="h2" style={{ fontFamily: 'inter', color: 'white'}}>{rocketData.height.meters}meters </Typography>
+      <Typography variant="h2" style={{ fontFamily: 'inter', color: 'white'}}>{rocketData.height.meters}meters </Typography>
+      <Typography variant="h2" style={{ fontFamily: 'inter', color: 'white'}}>{rocketData.height.meters}kg </Typography>
     </section>
 
-    {/*  description */}
+    <section className={styles.description}>
+    <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > {rocketData.description} </Typography>
+    </section>
+
+    {/* tables with all stages, engines and payload weights */}
+
+    <section className={styles.description}>
+      <Typography variant="h2" style={{ fontFamily: 'inter', color: 'white'}} > First Stage </Typography>
+      <div>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Thrust at Sea Level {rocketData.first_stage.thrust_sea_level.kN } kn</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Thrust Vacuum {rocketData.first_stage.thrust_vacuum.kN} kn</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Reusable: {rocketData.first_stage.reusable ? "true" : "false"}</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Engines: {rocketData.first_stage.engines}</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Fuel: {rocketData.first_stage.fuel_ammount_tons} </Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Burn Time: {rocketData.first_stage.burn_time_sec}s</Typography>
+       </div>
+    </section>
+
+    <section className={styles.description}>
+    <Typography variant="h2" style={{ fontFamily: 'inter', color: 'white'}} > Second Stage </Typography>
+    <div>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Thrust: {rocketData.second_stage.thrust.kN } kn</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Reusable: {rocketData.second_stage.reusable ? "true" : "false"}</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Engines: {rocketData.second_stage.engines}</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Fuel: {rocketData.second_stage.fuel_ammount_tons} </Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Burn Time: {rocketData.second_stage.burn_time_sec}s</Typography>
+       </div>
+    </section>
+
+    <section className={styles.description}>
+    <Typography variant="h2" style={{ fontFamily: 'inter', color: 'white'}} > Engines </Typography>
+    <div>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Type: {rocketData.engines.type }</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Number: {rocketData.engines.number ? "true" : "false"}</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Layout: {rocketData.engines.layout}</Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Propellant 1: {rocketData.engines.propellant_1} </Typography>
+       <Typography variant="p" style={{ fontFamily: 'inter', color: 'white'}} > Propellant 2: {rocketData.engines.propellant_2}</Typography>
+       </div>
+    </section>
+
+    <section className={styles.description}>
+    <Typography variant="h2" style={{ fontFamily: 'inter', color: 'white'}} > Payload Weights </Typography>
+    <div>
+      {rocketData.payload_weights.map((item, index) => (
+      <div key={index}>
+        <Typography variant="h6">{item.id}</Typography>
+        <Typography>Name: {item.name}</Typography>
+        <Typography>Kilos: {item.kg}</Typography>
+       </div>
+        ))}
+        </div>
+    </section>
+
+    <section>
+
+
+    <Typography variant="h2" style={{ fontFamily: 'inter', color: 'white'}} > Images </Typography>
+      {rocketData.flickr_images.map((image, index) => (
+        <img className={styles.image} key={index} src={image} alt={rocketData.name} />))}
+    </section>
+
+    </div>
   </div>
   : null
   }
 </main>;
 };
-
 
 export default Rocket;
